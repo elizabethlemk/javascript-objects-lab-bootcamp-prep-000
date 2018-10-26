ba493b2cd6f2543a0632ecaaa2c3ffababf3b889
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-10-26 16:15:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var recipes = new Object({
   recipes : 'object'
 })
 
-function updateObjectWithKeyAndValue(object, key, value ){
-  var obj = { prop: 1 }
+function updateObjectWithKeyAndValue(obj, key, value ){
   obj.prop2 = 2
   return obj
 }
